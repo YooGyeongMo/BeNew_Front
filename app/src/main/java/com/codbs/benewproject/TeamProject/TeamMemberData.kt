afc96f4dc42da66bfe5886be13ed3c67e46f640e
syunnnnn <122797072+syunnnnn@users.noreply.github.com>
2023-11-15 14:17:47 +0900

    Delete app directory

--- app/src/main/java/com/codbs/benewproject/TeamProject/TeamMemberData.kt
+++
@@ -1,4 +0,0 @@
-package com.codbs.benewproject.TeamProject
-
-class TeamMemberData(val member_name:String?=null) {
-}
--- app/src/main/java/com/codbs/benewproject/TodoList/ProjectData.kt
+++
@@ -1,4 +0,0 @@
-package com.codbs.benewproject.TodoList
-
-class ProjectData(val project_name:String?=null)  {
-}
