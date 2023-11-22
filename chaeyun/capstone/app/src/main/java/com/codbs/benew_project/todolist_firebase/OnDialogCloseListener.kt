2d955430a3328624694ff7bd344c6ffc9b611770
syunnnnn <codbs3128@naver.com>
2023-11-22 15:34:12 +0900

    레이아웃 디자인

--- chaeyun/capstone/app/src/main/java/com/codbs/benew_project/todolist_firebase/OnDialogCloseListener.kt
+++
@@ -1,7 +0,0 @@
-package com.codbs.benew_project.todolist_firebase
-
-import android.content.DialogInterface
-
-interface OnDialogCloseListener {
-    fun onDialogClose(dialogInterface: DialogInterface?)
-}
--- chaeyun/capstone/app/src/main/java/com/codbs/benew_project/todolist_firebase/ToDoModel.kt
+++
@@ -1,7 +0,0 @@
-package com.codbs.benew_project.todolist_firebase
-
-class ToDoModel: TaskId() {
-    val task: String? = null
-    val due: String? = null
-    val status = 0
-}
